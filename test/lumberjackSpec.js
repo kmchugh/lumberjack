29a53ad2f00fb40de7502c0059573367165e7939
Ken McHugh <kmchugh@ICatalyst.co.uk>
2014-06-15 15:12:22 +0800

    expanding jshint responsibility

--- test/lumberjackSpec.js
+++ test/lumberjackSpec.js
@@ -1,7 +1,10 @@
+'use strict';
+
 var expect = require('chai').expect;
 describe('lumberjack', function() {
     it('should always work', function() {
         var sut = require('../lib/lumberjack');
+        sut();
         expect(1).is.equal(1);
     });
 });
