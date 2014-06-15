ba1fed1e9e358b73d5f71eb7928061093756678c
Ken McHugh <kmchugh@ICatalyst.co.uk>
2014-06-15 14:52:42 +0800

    updates for code coverage

---
+++ test/lumberjackSpec.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+describe('lumberjack', function() {
+    it('should always work', function() {
+        var sut = require('../lib/lumberjack');
+        expect(1).is.equal(1);
+    });
+});
